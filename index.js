88fc12c569f0ae957c23a786573df7a71b3748a3
Gabriel N. Barta <62994558+GabNBar@users.noreply.github.com>
2023-04-08 13:40:43 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+function identity() {}
+
+function identityF() {}
+
+function add() {}
+
+function subtract() {}
+
+function multiply() {}
+
+function increment() {}
+
+function addF() {}
+
+function curry() {}
+
+function liftF() {}
+
+function twice() {}
+
+function composeU() {}
+
+function composeB() {}
+
+function limit() {}
+
+function from() {}
+
+function to() {}
+
+function fromTo() {}
+
+function element() {}
+
+function collect() {}
+
+function filter() {}
+
+function concat() {}
+
+function fibonacciF() {}
+
+function genSymF() {}
+
+function genSymFF() {}
+
+function counter() {}
+
+function revokable() {}
+
+module.exports = {
+  identity,
+  identityF,
+  add,
+  subtract,
+  multiply,
+  increment,
+  addF,
+  curry,
+  liftF,
+  twice,
+  composeU,
+  composeB,
+  limit,
+  from,
+  to,
+  fromTo,
+  element,
+  collect,
+  filter,
+  concat,
+  fibonacciF,
+  genSymF,
+  genSymFF,
+  counter,
+  revokable,
+};
